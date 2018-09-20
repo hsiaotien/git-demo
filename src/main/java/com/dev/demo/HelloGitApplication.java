3c8b0f7c47ad35d0857c188e0b334743864c162c
xiaotian.huang <hsiaotien@163.com>
2018-09-21 00:31:51 +0800

    Initial commit

---
+++ src/main/java/com/dev/demo/HelloGitApplication.java
@@ -0,0 +1,11 @@
+package com.dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitApplication.class,args);
+    }
+}
---
+++ src/main/java/com/dev/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.dev.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("git")
+    public String show(){
+        return "hello git";
+    }
+}
